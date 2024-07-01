eec2ec432405fd3252499dca1e56cc8ccdd4ad13
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-01 17:25:48 +0700

    Xây dựng phần chỉnh sửa phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -4,14 +4,14 @@ const systemConfig = require("../../config/system");
 
 
 // [GET] /admin/roles
-module.exports.index = async (req, res) => { 
+module.exports.index = async (req, res) => {
 
     let find = {
         deleted: false
     };
 
     const records = await Role.find(find);
-    
+
     res.render("admin/pages/roles/index.pug", {
         pageTitle: "Nhóm quyền",
         records: records
@@ -19,18 +19,59 @@ module.exports.index = async (req, res) => {
 };
 
 // [GET] /admin/roles/create
-module.exports.create = async (req, res) => { 
-    
+module.exports.create = async (req, res) => {
+
     res.render("admin/pages/roles/create.pug", {
         pageTitle: "Tạo nhóm quyền",
     });
 };
 
 // [POST] /admin/roles/create
-module.exports.createPost = async (req, res) => { 
+module.exports.createPost = async (req, res) => {
 
     const record = new Role(req.body);
     await record.save();
 
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
 };
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const find = {
+            _id: id,
+            deleted: false
+        }
+
+        const data = await Role.findOne(find);
+
+        res.render("admin/pages/roles/edit.pug", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            data: data
+        });
+
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne({_id: id}, req.body);
+    
+        req.flash("success", "Cập nhật nhóm quyền thành công!");
+        
+    } catch (error) {
+        req.flash("error", "Cập nhật nhóm quyền thất bại!");
+    }
+    res.redirect("back");
+
+};
+
