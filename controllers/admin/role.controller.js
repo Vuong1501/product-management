f9626239c73de048c7312f745d834d076bc15e97
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-04 15:09:00 +0700

    Xây dựng phần phân quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -75,3 +75,29 @@ module.exports.editPatch = async (req, res) => {
 
 };
 
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+
+    let find = {
+        deleted: false
+    };
+    const records = await Role.find(find);
+    res.render("admin/pages/roles/permissions.pug", {
+        pageTitle: "Phân quyền",
+        records: records
+    });
+};
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+
+    const permissions = JSON.parse(req.body.permissions);
+
+    for (const item of permissions) {
+        await Role.updateOne({_id: item.id}, {permissions: item.permissions});
+    }
+
+    req.flash("success", "Cập nhật phân quyền thành công!");
+
+    res.redirect("back");
+};
