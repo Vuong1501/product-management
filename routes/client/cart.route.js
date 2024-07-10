2791e4e9b8a1347d4e99c1a5c95c75607366be01
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-10 22:40:56 +0700

    Làm trang giỏ hàng

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router(); 
 
 const controller = require("../../controllers/client/cart.controller"); // import từ file tên là home.controller.js
+
+router.get("/", controller.index);
 router.post('/add/:productId', controller.addPost); // từ hàm controller gọi đến hàm index
+
 
 module.exports = router; 
