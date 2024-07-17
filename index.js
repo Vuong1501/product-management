ede6c20073e3c0354bfe0e43f61aa7c1718df978
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-17 20:52:54 +0700

    Làm trang 404

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce
 routeAdmin(app);
 route(app);
 
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+      pageTitle: "404 Not Found",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
