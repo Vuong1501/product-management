c169fb3acaec759abfaf7db9741b7bb0ba61d4aa
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-18 22:37:34 +0700

    fix chat realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,15 +20,19 @@ if(formSendData){
 
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
-    const body = document.querySelector(".chat .inner-body") // lấy ra thẻ body
+    const body = document.querySelector(".chat .inner-body"); // lấy ra thẻ body
+
+    const div = document.createElement("div");    //add thêm 1 thẻ div
 
     let htmlFullName = "";
-    if(myId = data.userId){ // nếu là cùng 1 người thì sẽ ẩn tên
+
+    if(myId == data.userId){ // nếu là cùng 1 người thì sẽ ẩn tên
         div.classList.add("inner-outgoing");//add thêm class inner-coming vào thể div đó
     } else { // khác nhau sẽ hiện tên
         div.classList.add("inner-incoming");//add thêm class inner-coming vào thể div đó
+        htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
     }
-    //add thêm 1 thẻ div
+
     div.innerHTML = `
         ${htmlFullName}
         <div class="inner-content">${data.content}</div> 
