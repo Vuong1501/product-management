7bf82f4911a8926da3a776e060796774fd950720
Vuong1501 <vuong.44.10a5@gmail.com>
2024-07-18 21:56:41 +0700

    Làm tính năng chat cơ bản

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData){
+    formSendData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value; // lấy ra tin nhắn vừa gửi
+
+        if(content){
+            socket.emit("CLIENT_SEND_MESSAGE", content) // gửi lên sự kiện tên là CLIENT_SEND_MESSAGE với nội dung là content
+            e.target.elements.content.value = ""; // gán lại ô input sang rỗng
+        }
+    });
+}
+
+// END CLIENT_SEND_MASSAGE
