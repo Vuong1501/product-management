40090b9b9e9a85e2e722af5b4b70ea7a9a23396e
Vuong1501 <vuong.44.10a5@gmail.com>
2024-08-05 16:22:21 +0700

    Tạo phòng chat

--- routes/client/rooms-chat.route.js
+++ routes/client/rooms-chat.route.js
@@ -5,6 +5,10 @@ const controller = require("../../controllers/client/rooms-chat.controller");
 
 router.get("/", controller.index);
 
+router.get("/create", controller.create);
+
+router.post("/create", controller.createPost);
+
 
 
 module.exports = router
